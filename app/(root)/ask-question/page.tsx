42a329c8d531dcdabc4da2ea0175d6a620fddb0b
Nitish Pandit <appgurunitish@gmail.com>
2024-04-28 10:23:53 +0530

    fixed error of webhook

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+// import { auth } from "@clerk/nextjs";
+// import { redirect } from "next/navigation";
 import Question from "@/components/form/Question";
 import { getUserById } from "@/lib/action/user.action";
 
 const AskQuestion = async () => {
-  const { userId } = auth();
+  // const { userId } = auth();
 
-  if (!userId) redirect("/sign-in");
+  // if (!userId) redirect("/sign-in");
 
-  console.log(userId);
+  // console.log(userId);
+
+  const userId = "clerk001";
 
   const mongoUser = await getUserById({ userId });
-
-  console.log("MongoUser", mongoUser);
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
 
       <div className="mt-9">
-        <Question />
+        <Question author={JSON.stringify(mongoUser._id)} />
       </div>
     </div>
   );
